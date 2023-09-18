64b90206d04c827a2d209256c66eb697f26452ea
Vaneshik <maksimchusovlyanov@yandex.ru>
2023-09-18 15:58:41 +0300

    java/lab1 #brrrr2

--- java/lab1/Main.java
+++ java/lab1/Main.java
@@ -20,14 +20,10 @@ public class Main {
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 13; j++) {
                 arr[i][j] = switch ((int) c[i]) {
-                    case 5:
+                    case 5, 11, 13:
                         yield Math.log(Math.pow(Math.sin(Math.sin(Math.pow((x[j] + 0.5), 2))), 2));
                     case 9:
                         yield Math.log(Math.pow((2 * (Math.PI / (2 + Math.sqrt(Math.abs(x[j]))))), 2));
-                    case 11:
-                        yield Math.log(Math.pow(Math.sin(Math.sin(Math.pow((x[j] + 0.5), 2))), 2));
-                    case 13:
-                        yield Math.log(Math.pow(Math.sin(Math.sin(Math.pow((x[j] + 0.5), 2))), 2));
                     default:
                         yield Math.atan(0.25 * Math.cos(Math.log(Math.abs(x[j]) / 2)));
                 };
