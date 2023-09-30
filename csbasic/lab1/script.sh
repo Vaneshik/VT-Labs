4ed5be99f5541dd5a3eedc5f83361b538856ff9a
Vaneshik <maksimchusovlyanov@yandex.ru>
2023-09-30 11:22:39 +0300

    csbasic/lab1 #absolute paths

--- csbasic/lab1/script.sh
+++ csbasic/lab1/script.sh
@@ -1,116 +1,109 @@
 #!/usr/bin/bash
 # variant 1415
 
-chmod -R 777 lab0  
-rm -rf lab0
+chmod -R 777 ~/VT-Labs/csbasic/lab1/lab0  
+rm -rf ~/VT-Labs/csbasic/lab1/lab0/
 
 # enable double asterisk
 shopt -s globstar
 
 # ================ PART 1 =================
 # Create tree and write content
-mkdir lab0 && cd lab0
-    mkdir bulbasaur4 && cd bulbasaur4 
-        echo -e "Тип покемона  ROCK WATER" > omastar
-	    echo -e "Тип покемона  WATER\nNONE" > seel
-	    echo -e "Возможности  Overland=2 Surface=1 Sky=7 Jump=1 Power1=0\nIntelligence=4 Tracker=0" > woobat
-	    echo -e"satk=7 sdef=3 spd=6" > horsea
-	    echo -e "Живет\nForest Urban"> persian
-        cd ..
-
-    echo -e "satk=13 sdef=12 spd=8" > gardevoir0
-    echo -e "Ходы  Bullet\nSeed Covet Fury Cutter Giga Drain Heal Bell Helping Hand Hyper\nVoice Irot Tail Knock Off Last Resort Mud-Slap Seed Bomb Sleep Talk Snore Swift Sunthesis Worry Seed" > leafeon9
-    
-    mkdir nosepass8 && cd nosepass8
-        mkdir grumpig kingler
-        echo -e "Ходы  Bullet Seed Giga Drain\nHelping Hand Natural Gift Rollout Seed Bomb Sleep Talk Snore Synthesis\nWorry Seed" > cherrim
-        echo -e "weight=92.6 height=55.0 atk=12\ndef=8" > luxray
-        cd ..
-    
-    echo -e "Возможности  Overland=1 Surface=5 Underwater=5 Sky=5\nJump=2 Power=2 Intelligence=4 Fountain=0 Gilled=0" > staryu5
-
-    mkdir ursaring8 && cd ursaring8
-        mkdir munna seedot
-        echo -e "Возможности\nOverland=7 Surface=6 Jump=3 Power=5\nIntelligence=4" > exploud
-        echo -e "Способности  Sand-Attack Thundershock Quick\nAttack Double Kick Thunder Fang Pin Missile Agility Thunder Wave\nDischarge Last Resort Thunder" > jolteon
-        echo -e "Развитые способности  Super\nLuck" > togekiss
-	cd ..
+mkdir ~/VT-Labs/csbasic/lab1/lab0/
+    mkdir ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/ 
+        echo -e "Тип покемона  ROCK WATER" > ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/omastar
+	    echo -e "Тип покемона  WATER\nNONE" > ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/seel
+	    echo -e "Возможности  Overland=2 Surface=1 Sky=7 Jump=1 Power1=0\nIntelligence=4 Tracker=0" > ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/woobat
+	    echo -e"satk=7 sdef=3 spd=6" > ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/horsea
+	    echo -e "Живет\nForest Urban"> ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/persian
+    echo -e "satk=13 sdef=12 spd=8" > ~/VT-Labs/csbasic/lab1/lab0/gardevoir0
+    echo -e "Ходы  Bullet\nSeed Covet Fury Cutter Giga Drain Heal Bell Helping Hand Hyper\nVoice Irot Tail Knock Off Last Resort Mud-Slap Seed Bomb Sleep Talk Snore Swift Sunthesis Worry Seed" > ~/VT-Labs/csbasic/lab1/lab0/leafeon9
+    mkdir ~/VT-Labs/csbasic/lab1/lab0/nosepass8/
+        mkdir ~/VT-Labs/csbasic/lab1/lab0/nosepass8/grumpig ~/VT-Labs/csbasic/lab1/lab0/nosepass8/kingler
+        echo -e "Ходы  Bullet Seed Giga Drain\nHelping Hand Natural Gift Rollout Seed Bomb Sleep Talk Snore Synthesis\nWorry Seed" > ~/VT-Labs/csbasic/lab1/lab0/nosepass8/cherrim
+        echo -e "weight=92.6 height=55.0 atk=12\ndef=8" > ~/VT-Labs/csbasic/lab1/lab0/nosepass8/luxray
+    echo -e "Возможности  Overland=1 Surface=5 Underwater=5 Sky=5\nJump=2 Power=2 Intelligence=4 Fountain=0 Gilled=0" > ~/VT-Labs/csbasic/lab1/lab0/staryu5
+    mkdir ~/VT-Labs/csbasic/lab1/lab0/ursaring8/
+        mkdir ~/VT-Labs/csbasic/lab1/lab0/ursaring8/munna ~/VT-Labs/csbasic/lab1/lab0/ursaring8/seedot
+        echo -e "Возможности\nOverland=7 Surface=6 Jump=3 Power=5\nIntelligence=4" > ~/VT-Labs/csbasic/lab1/lab0/ursaring8/exploud
+        echo -e "Способности  Sand-Attack Thundershock Quick\nAttack Double Kick Thunder Fang Pin Missile Agility Thunder Wave\nDischarge Last Resort Thunder" > ~/VT-Labs/csbasic/lab1/lab0/ursaring8/jolteon
+        echo -e "Развитые способности  Super\nLuck" > ~/VT-Labs/csbasic/lab1/lab0/ursaring8/togekiss
 # =========================================
 
 
 # ================ PART 2 =================
 # Set permissions
-chmod 771 bulbasaur4
-chmod a=r bulbasaur4/omastar
-chmod 006 bulbasaur4/seel
-chmod 444 bulbasaur4/woobat
-chmod 004 bulbasaur4/horsea
-chmod 660 bulbasaur4/persian
+chmod 771 ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4
+chmod a=r ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/omastar
+chmod 006 ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/seel
+chmod 444 ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/woobat
+chmod 004 ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/horsea
+chmod 660 ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/persian
 
-chmod 046 gardevoir0
-chmod 062 leafeon9
+chmod 046 ~/VT-Labs/csbasic/lab1/lab0/gardevoir0
+chmod 062 ~/VT-Labs/csbasic/lab1/lab0/leafeon9
 
-chmod 733 nosepass8
-chmod 711 nosepass8/grumpig
-chmod 577 nosepass8/kingler
-chmod 440 nosepass8/cherrim
-chmod 622 nosepass8/luxray
+chmod 733 ~/VT-Labs/csbasic/lab1/lab0/nosepass8
+chmod 711 ~/VT-Labs/csbasic/lab1/lab0/nosepass8/grumpig
+chmod 577 ~/VT-Labs/csbasic/lab1/lab0/nosepass8/kingler
+chmod 440 ~/VT-Labs/csbasic/lab1/lab0/nosepass8/cherrim
+chmod 622 ~/VT-Labs/csbasic/lab1/lab0/nosepass8/luxray
 
-chmod 624 staryu5
+chmod 624 ~/VT-Labs/csbasic/lab1/lab0/staryu5
 
-chmod 311 ursaring8
-chmod 357 ursaring8/munna
-chmod 524 ursaring8/seedot
-chmod 620 ursaring8/exploud
-chmod 604 ursaring8/jolteon
-chmod 600 ursaring8/togekiss
+chmod 311 ~/VT-Labs/csbasic/lab1/lab0/ursaring8
+chmod 357 ~/VT-Labs/csbasic/lab1/lab0/ursaring8/munna
+chmod 524 ~/VT-Labs/csbasic/lab1/lab0/ursaring8/seedot
+chmod 620 ~/VT-Labs/csbasic/lab1/lab0/ursaring8/exploud
+chmod 604 ~/VT-Labs/csbasic/lab1/lab0/ursaring8/jolteon
+chmod 600 ~/VT-Labs/csbasic/lab1/lab0/ursaring8/togekiss
 # =========================================
 
 
 # ================ PART 3 =================
 # Copy and make links
-ln -s gardevoir0 ./bulbasaur4/seelgardevoir
+ln -s ~/VT-Labs/csbasic/lab1/lab0/gardevoir0 ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/seelgardevoir
 
-chmod u+r leafeon9
-chmod u+w nosepass8/kingler
-cp leafeon9 ./nosepass8/kingler
-cp leafeon9 ./bulbasaur4/seelleafeon
-chmod u-w nosepass8/kingler
-chmod u-r leafeon9
+chmod u+r ~/VT-Labs/csbasic/lab1/lab0/leafeon9
+chmod u+w ~/VT-Labs/csbasic/lab1/lab0/nosepass8/kingler
+cp ~/VT-Labs/csbasic/lab1/lab0/leafeon9 ~/VT-Labs/csbasic/lab1/lab0/nosepass8/kingler
+cp ~/VT-Labs/csbasic/lab1/lab0/leafeon9 ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/seelleafeon
+chmod u-w ~/VT-Labs/csbasic/lab1/lab0/nosepass8/kingler
+chmod u-r ~/VT-Labs/csbasic/lab1/lab0/leafeon9
 
-ln leafeon9 ./bulbasaur4/omastarleafeon
-cat ./bulbasaur4/omastar ./nosepass8/cherrim > ./staryu5_66
-ln -s nosepass8 ./Copy_64
+ln ~/VT-Labs/csbasic/lab1/lab0/leafeon9 ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/omastarleafeon
+cat ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/omastar ~/VT-Labs/csbasic/lab1/lab0/nosepass8/cherrim > ~/VT-Labs/csbasic/lab1/lab0/staryu5_66
+ln -s ~/VT-Labs/csbasic/lab1/lab0/nosepass8 ~/VT-Labs/csbasic/lab1/lab0/Copy_64
 
-chmod u+w ursaring8/seedot
-cp -R nosepass8 ./ursaring8/seedot
-chmod u-w ursaring8/seedot
+chmod u+w ~/VT-Labs/csbasic/lab1/lab0/ursaring8/seedot
+cp -R ~/VT-Labs/csbasic/lab1/lab0/nosepass8 ~/VT-Labs/csbasic/lab1/lab0/ursaring8/seedot
+chmod u-w ~/VT-Labs/csbasic/lab1/lab0/ursaring8/seedot
 # =========================================
 
 
 # ================ PART 4 =================
 # Search and filter
 echo "======== 4.1 ========"
-wc -m <staryu5 >>staryu5 2>&1 && echo "Done!"
+wc -m <~/VT-Labs/csbasic/lab1/lab0/staryu5 >>~/VT-Labs/csbasic/lab1/lab0/staryu5 2>&1 && echo "Done!"
 echo "======== 4.2 ========"
 ls -Rl 2>/tmp/s409858_errors.txt | grep "on" | sort -n -k 2
 echo "======== 4.3 ========"
 cat $(ls -dp1 "$PWD/"**/* | grep -E "(/[^/]+)+/l[^/:]+\$") | sort -r
 echo "======== 4.4 ========"
-cat bulbasaur4/* | grep -v "Sle"
+cat ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/* | grep -v "Sle"
 echo "======== 4.5 ========"
 ls -tdp1l "$PWD/"**/* 2>&1 | grep -E "(/[^/]+)+/g.+\$"
 echo "======== 4.6 ========"
-cat ursaring8/exploud ursaring8/jolteon | grep ce
+cat ~/VT-Labs/csbasic/lab1/lab0/ursaring8/exploud ~/VT-Labs/csbasic/lab1/lab0/ursaring8/jolteon | grep ce
 # =========================================
 
 
 # ================ PART 5 =================
 # delete files
-rm -f leafeon9
-rm -rf ./ursaring8/exploud
-rm -f ./bulbasaur4/seelgardevo*
-rm -f ./bulbasaur4/omastarleafe*
-chmod -R 777 ursaring8 && rm -rf ursaring8
-# rm -rf ursaring8/seedot
+rm -f ~/VT-Labs/csbasic/lab1/lab0/leafeon9
+rm -rf ~/VT-Labs/csbasic/lab1/lab0/ursaring8/exploud
+rm -f ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/seelgardevo*
+rm -f ~/VT-Labs/csbasic/lab1/lab0/bulbasaur4/omastarleafe*
+chmod -R 777 ~/VT-Labs/csbasic/lab1/lab0/ursaring8 && rm -rf ~/VT-Labs/csbasic/lab1/lab0/ursaring8
+# rm -rf ~/VT-Labs/csbasic/lab1/lab0/ursaring8/seedot
 # =========================================
